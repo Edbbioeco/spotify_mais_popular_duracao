f2fcc786a6910bd1e8cf170bf028d42d3d89b76d
Edbbioeco <edsonbbiologia@gmail.com>
2025-12-29 20:52:32 -0300

    Análise exploratória inicial dos dados.
    Atualização: 29.12.2025 20:53

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -2,6 +2,8 @@
 
 library(tidyverse)
 
+library(magrittr)
+
 # Dados ----
 
 ## Importando ----
@@ -13,3 +15,32 @@ dados <- readr::read_csv("spotify_songs.csv")
 dados
 
 dados |> dplyr::glimpse()
+
+## Tratando ----
+
+# Histograma dos valores numérciso ----
+
+## Nomes das variáveis ----
+
+nomes_var <- dados |> 
+  dplyr::select_if(is.numeric) |> 
+  names()
+
+nomes_var
+
+## Loop para todas as variáveis ----
+
+histo_vars <- function(nomes_var){
+  
+  ggplt <- dados |> 
+    dplyr::select(nomes_var) |> 
+    ggplot(aes(dados[[nomes_var]])) +
+    geom_histogram(color = "black") +
+    labs(title = nomes_var,
+         x = nomes_var)
+  
+  print(ggplt)
+  
+}
+
+purrr::walk(nomes_var, histo_vars)
